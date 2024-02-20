5a29b45e158f62a179eb090e162edc0f117adf5c
Necmi Gunduz <necmigunduz@gmail.com>
2024-02-20 18:39:04 +0300

    fix: style with prettier

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,39 @@
 <script setup>
-import { ref, onMounted, computed, watch } from 'vue'
+import { ref, onMounted, computed, watch } from "vue";
 
-const todos = ref([])
-const name = ref('')
+const todos = ref([]);
+const name = ref("");
 
-const input_content = ref('')
-const input_category = ref(null)
-const responsible = ref('')
+const input_content = ref("");
+const input_category = ref(null);
+const responsible = ref("");
 
-const todos_asc = computed(() => todos.value.sort((a, b) => {
-  return a.createdAt - b.createdAt
-}))
+const todos_asc = computed(() =>
+  todos.value.sort((a, b) => {
+    return a.createdAt - b.createdAt;
+  })
+);
 
 watch(name, (newVal) => {
-  localStorage.setItem('name', newVal)
-})
+  localStorage.setItem("name", newVal);
+});
 
-watch(todos, (newVal) => {
-  localStorage.setItem('todos', JSON.stringify(newVal))
-}, { deep: true })
+watch(
+  todos,
+  (newVal) => {
+    localStorage.setItem("todos", JSON.stringify(newVal));
+  },
+  { deep: true }
+);
 
 const addTodo = () => {
-  if (input_content.value.trim() === '' || input_category.value === null || responsible.value.trim() === '') {
-    alert("Content, category, or responsible is missing!")
-    return
+  if (
+    input_content.value.trim() === "" ||
+    input_category.value === null ||
+    responsible.value.trim() === ""
+  ) {
+    alert("Content, category, or responsible is missing!");
+    return;
   }
 
   todos.value.push({
@@ -32,54 +42,80 @@ const addTodo = () => {
     responsible: responsible.value,
     done: false,
     editable: false,
-    createdAt: new Date().getTime()
-  })
+    createdAt: new Date().getTime(),
+  });
 
   // Reset values
-  input_category.value = null
-  input_content.value = ''
-  responsible.value = ''
-}
+  input_category.value = null;
+  input_content.value = "";
+  responsible.value = "";
+};
 
-const removeTodo = todo => {
-  todos.value = todos.value.filter(t => t !== todo)
-}
+const removeTodo = (todo) => {
+  todos.value = todos.value.filter((t) => t !== todo);
+};
 
 onMounted(() => {
-  name.value = localStorage.getItem('name') || ''
-  todos.value = JSON.parse(localStorage.getItem('todos')) || []
-})
+  name.value = localStorage.getItem("name") || "";
+  todos.value = JSON.parse(localStorage.getItem("todos")) || [];
+});
 </script>
 
 <template>
   <main class="app">
     <section class="greeting">
       <h2 class="title">
-        How are you doing today,<input type="text" placeholder="your name here" v-model="name" />
+        How are you doing today,<input
+          type="text"
+          placeholder="your name here"
+          v-model="name"
+        />
       </h2>
     </section>
     <section class="create-todo">
       <h3>Create a Todo</h3>
       <form @submit.prevent="addTodo">
         <h4>Todo content</h4>
-        <input type="text" name="content" id="content" placeholder="e.g. make a video for your blog..."
-          v-model="input_content" />
+        <input
+          type="text"
+          name="content"
+          id="content"
+          placeholder="e.g. make a video for your blog..."
+          v-model="input_content"
+        />
         <h4>Pick a category</h4>
         <div class="options">
           <label>
-            <input type="radio" name="category" id="category1" value="business" v-model="input_category" />
+            <input
+              type="radio"
+              name="category"
+              id="category1"
+              value="business"
+              v-model="input_category"
+            />
             <span class="bubble business"></span>
             <div>Business</div>
           </label>
           <label>
-            <input type="radio" name="category" id="category2" value="personal" v-model="input_category" />
+            <input
+              type="radio"
+              name="category"
+              id="category2"
+              value="personal"
+              v-model="input_category"
+            />
             <span class="bubble personal"></span>
             <div>Personal</div>
           </label>
         </div>
         <h4>Responsible</h4>
-        <input type="text" name="responsible" id="responsible" placeholder="e.g. Alex, Alicia, ..."
-          v-model="responsible" />
+        <input
+          type="text"
+          name="responsible"
+          id="responsible"
+          placeholder="e.g. Alex, Alicia, ..."
+          v-model="responsible"
+        />
         <input type="submit" value="Create a todo" />
       </form>
     </section>
@@ -87,22 +123,27 @@ onMounted(() => {
     <section class="todo-list">
       <h3>TODO LIST</h3>
       <div class="list">
-        <div v-for='todo in todos_asc' :class="`todo-item ${todo.done && 'done'}`">
+        <div
+          v-for="todo in todos_asc"
+          :class="`todo-item ${todo.done && 'done'}`"
+        >
           <label>
             <input type="checkbox" v-model="todo.done" />
             <span :class="`bubble ${todo.category}`"></span>
           </label>
           <div class="todo-content">
-            <input type="text" v-model="todo.content">
+            <input type="text" v-model="todo.content" />
           </div>
           <div class="todo-category">
-            <input type="text" v-model="todo.category">
+            <input type="text" v-model="todo.category" />
           </div>
           <div class="responsible">
-            <input type="text" v-model="todo.responsible">
+            <input type="text" v-model="todo.responsible" />
           </div>
           <div class="actions">
-            <button class="delete" @click="removeTodo(todo)">Delete this todo</button>
+            <button class="delete" @click="removeTodo(todo)">
+              Delete this todo
+            </button>
           </div>
         </div>
       </div>
